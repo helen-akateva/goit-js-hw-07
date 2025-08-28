97f3a0541768b29d3474c7cb4263b81607451054
helen-akateva <akateva17@gmail.com>
2025-08-28 16:12:29 +0300

    task-2

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,7 @@
 const categoriesListEl = document.querySelector("ul#categories");
 const items = categoriesListEl.querySelectorAll("li.item");
 console.log(`Number of categories:`, items.length);
-items.forEach(function (item) {
+items.forEach((item) => {
   const titleEl = item.querySelector("h2");
   const categoryElText = titleEl.textContent;
   const listItemsEl = item.querySelectorAll("ul li");
