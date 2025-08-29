8604d251fe2a649c23bd7d509fcc715c69b4e90e
helen-akateva <akareva17@gmail.com>
2025-08-29 11:46:34 +0300

    update

--- js/task-1.js
+++ js/task-1.js
@@ -3,9 +3,9 @@ const items = categoriesListEl.querySelectorAll("li.item");
 console.log(`Number of categories:`, items.length);
 items.forEach((item) => {
   const titleEl = item.querySelector("h2");
-  const categoryElText = titleEl.textContent;
+  const categoryNameEl = titleEl.textContent;
   const listItemsEl = item.querySelectorAll("ul li");
-  const elementsCountEl = listItemsEl.length;
-  console.log(`Category:`, categoryElText);
-  console.log(`Elements:`, elementsCountEl);
+  const elementsCount = listItemsEl.length;
+  console.log(`Category:`, categoryNameEl);
+  console.log(`Elements:`, elementsCount);
 });
