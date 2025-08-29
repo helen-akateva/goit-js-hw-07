672419d3741a5102ce63d2595eaa07d60aecfccd
helen-akateva <akateva17@gmail.com>
2025-08-29 18:43:23 +0300

    update

--- js/task-4.js
+++ js/task-4.js
@@ -1,10 +1,30 @@
 const registerForm = document.querySelector(".login-form");
+const emailInput = document.querySelector('input[name="email"]');
+const passwordInput = document.querySelector('input[name="password"]');
+
+emailInput.addEventListener("focus", () => {
+  if (!emailInput.value) emailInput.placeholder = "Type area";
+});
+
+passwordInput.addEventListener("focus", () => {
+  if (!passwordInput.value) passwordInput.placeholder = "Type area";
+});
+
+emailInput.addEventListener("blur", () => {
+  emailInput.placeholder = "";
+});
+
+passwordInput.addEventListener("blur", () => {
+  passwordInput.placeholder = "";
+});
 
 registerForm.addEventListener("submit", handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault();
+
   const form = event.target;
+
   const email = form.elements.email.value.trim();
   const password = form.elements.password.value.trim();
 
--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector(".change-color");
+const spanEl = document.querySelector(".color");
+
+const handleClick = (event) => {
+  const color = getRandomHexColor();
+  const bodyEl = document.querySelector("body");
+
+  bodyEl.style.backgroundColor = color;
+  spanEl.textContent = color;
+};
+
+button.addEventListener("click", handleClick);
