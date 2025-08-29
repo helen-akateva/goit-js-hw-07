330eb3f8e76e4ee371b6e598a043343085e85640
helen-akateva <akareva17@gmail.com>
2025-08-29 14:26:29 +0300

    update

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,14 @@
 const categoriesListEl = document.querySelector("ul#categories");
 const items = categoriesListEl.querySelectorAll("li.item");
+
 console.log(`Number of categories:`, items.length);
+
 items.forEach((item) => {
   const titleEl = item.querySelector("h2");
   const categoryNameEl = titleEl.textContent;
   const listItemsEl = item.querySelectorAll("ul li");
   const elementsCount = listItemsEl.length;
+
   console.log(`Category:`, categoryNameEl);
   console.log(`Elements:`, elementsCount);
 });
